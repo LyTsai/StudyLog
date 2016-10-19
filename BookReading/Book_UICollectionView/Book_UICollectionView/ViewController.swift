1f9340073f957a060621f58a9fe79df26bf294d6
LyTsai <imac@iMac.local>
2016-10-19 14:41:09 +0800

    add

--- BookReading/Book_UICollectionView/Book_UICollectionView/ViewController.swift
+++ BookReading/Book_UICollectionView/Book_UICollectionView/ViewController.swift
@@ -50,23 +50,34 @@ class SampleOneViewController: UIViewController {
 // MARK: ------------------- Two -----------------------
 class SampleTwoCell: UICollectionViewCell {
     
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        text = "0"
-//    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        text = "Hello World"
+    }
     
-    var text = "Hello World"
+    var text = "Hello World" {
+        willSet{
+            textLabel.text = newValue
+        }
+    }
     
     private let textLabel = UILabel()
     
-    func getCellWithText(text: String) -> SampleTwoCell {
-        let cell = SampleTwoCell()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    func setupUI() {
         textLabel.frame = bounds
-        textLabel.text = text
+        textLabel.textAlignment = .Center
         contentView.addSubview(textLabel)
         backgroundColor = UIColor.whiteColor()
-        
-        return cell
     }
 }
 
@@ -74,7 +85,6 @@ class SampleTwoViewController: UICollectionViewController {
     let CellIdentifier = "Cell Identifier"
     var datesArray = [NSDate]()
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -82,7 +92,7 @@ class SampleTwoViewController: UICollectionViewController {
         flowLayout.minimumInteritemSpacing = 40
         flowLayout.minimumLineSpacing = 40
         flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        flowLayout.itemSize = CGSize(width: 200, height: 200)
+        flowLayout.itemSize = CGSize(width: 150, height: 80)
         collectionView?.indicatorStyle = .White
         
         collectionView?.registerClass(SampleTwoCell.self, forCellWithReuseIdentifier: CellIdentifier)
@@ -97,8 +107,7 @@ class SampleTwoViewController: UICollectionViewController {
         dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("h:mm:ss a", options: 0, locale: NSLocale.currentLocale())
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(CellIdentifier, forIndexPath: indexPath) as! SampleTwoCell
-        let string = dateFormatter.stringFromDate(datesArray[indexPath.item])
-        cell.getCellWithText(string)
+        cell.text = dateFormatter.stringFromDate(datesArray[indexPath.item])
         
         return cell
     }
@@ -107,10 +116,114 @@ class SampleTwoViewController: UICollectionViewController {
         collectionView?.performBatchUpdates({ 
             let dateNow = NSDate()
             self.datesArray.insert(dateNow, atIndex: 0)
-            print("hello")
             
             self.collectionView?.insertItemsAtIndexPaths([NSIndexPath.init(forRow: 0, inSection: 0)])// (0, 0)
             }, completion: nil)
     }
+}
+
+// MARK: ------------------- Two -----------------------
+class SampleThreeViewController: UICollectionViewController {
+    let CellIdentifier = "Cell Identifier"
     
+    var imageArray = [UIImage?]()
+    var colorArray = [UIColor]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupModels()
+        setupLayout()
+        
+        collectionView?.registerClass(SampleThreeCell.self, forCellWithReuseIdentifier: CellIdentifier)
+        collectionView?.allowsMultipleSelection = true
+        collectionView?.indicatorStyle = .White
+        
+//        collectionView?.canCancelContentTouches = false
+//        collectionView?.delaysContentTouches = false
+    }
+    
+    func setupModels() {
+        for i in 0..<12 {
+            let image = UIImage(named: "icon\(i)")
+            imageArray.append(image)
+        }
+        
+        for _ in 0..<10 {
+            let redValue = CGFloat(arc4random())%255/255.0
+            let greenValue = CGFloat(arc4random())%255/255.0
+            let blueValue = CGFloat(arc4random())%255/255.0
+            colorArray.append(UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1))
+        }
+    }
+    
+    func setupLayout() {
+        let flowLayout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.minimumInteritemSpacing = 20
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+    }
+    
+    // dataSource
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return colorArray.count
+    }
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(CellIdentifier, forIndexPath: indexPath) as! SampleThreeCell
+        
+        cell.image = imageArray[indexPath.row]
+        cell.backgroundColor = colorArray[indexPath.section]
+        
+        return cell
+    }
+}
+
+class SampleThreeCell: UICollectionViewCell {
+    var image: UIImage! {
+        willSet{
+            imageView.image = newValue
+        }
+    }
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    func setupUI() {
+        backgroundColor = UIColor.whiteColor()
+        
+        imageView.frame = CGRectInset(bounds, 10, 10)
+        contentView.addSubview(imageView)
+        
+        let selectedBackgroundView = UIView(frame: CGRectZero)
+        selectedBackgroundView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.7)
+        self.selectedBackgroundView = selectedBackgroundView
+    }
+    
+    override var highlighted: Bool {
+        willSet{
+            if newValue == true {
+                imageView.alpha = 0.8
+            }else {
+                imageView.alpha = 1
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        backgroundColor = UIColor.whiteColor()
+        image = nil
+    }
 }
