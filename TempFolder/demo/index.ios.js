26cd3b53d9835571d14734a6840207998ae1b680
LyTsai <imac@iMac.local>
2017-04-05 17:35:05 +0800

    adjust

--- TempFolder/demo/index.ios.js
+++ TempFolder/demo/index.ios.js
@@ -1,54 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class demo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+class HelloWorld extends Component {
+    render() {
+        return (
+                <View style={styles.container}>
+                <Text style={styles.welcome}>
+                HelloWorld !!
+                </Text>
+                </View>
+                );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('demo', () => demo);
+
+const styles = StyleSheet.create({
+                                 container: {
+                                 flex: 1,
+                                 justifyContent: 'center',
+                                 alignItems: 'center'
+                                 },
+                                 welcome: {
+                                 fontSize: 25,
+                                 textAlign: 'center'
+                                 }
+                                 });
+
+AppRegistry.registerComponent('demo', () => HelloWorld);
