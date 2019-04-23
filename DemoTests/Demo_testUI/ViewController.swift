76d73f57f492e04682ab16cf8f3da767d1fb675f
L <iMac@Ls-iMac.local>
2019-04-23 17:47:15 +0800

    add

--- DemoTests/Demo_testUI/ViewController.swift
+++ DemoTests/Demo_testUI/ViewController.swift
@@ -14,14 +14,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let decoLabel = UILabel(frame: view.bounds)
+        decoLabel.numberOfLines = 0
+        decoLabel.font = UIFont.systemFont(ofSize: 22 * fontFactor)
+        
         let label = UILabel(frame: view.bounds)
+        label.textColor = UIColor.white
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 22 * fontFactor)
+        view.addSubview(decoLabel)
+        
         view.addSubview(label)
-        let text = "By creating an account, I agree to AnnielyticX’s terms of use and privacy policy."
+        let text = "By creating an account, I agree to AnnielyticX’s.By creating an account, By creating an account, By creating an account"
+        label.text = text
         
-        let attributedText = NSMutableAttributedString(string: text, attributes: [.font: UIFont.systemFont(ofSize: 12 * fontFactor)])
-        attributedText.addAttributes([NSAttributedString.Key.link: "netdown"], range: NSMakeRange(0, 5))
+         let attributedText = NSMutableAttributedString(string: text, attributes: [.strokeWidth: NSNumber(value: 3)])
         
-        label.attributedText = attributedText
+        let subString = "By"
+        var startIndex = text.startIndex
+        while let range = text.range(of: subString, options: .caseInsensitive, range: startIndex..<text.endIndex, locale: nil) {
+            attributedText.addAttributes([.strokeWidth: NSNumber(value: -20)], range: NSMakeRange(range.upperBound.utf16Offset(in: text), 2))
+            
+            startIndex = range.upperBound
+        }
+        
+//        text. var result: [Index] = []
+//        var start = startIndex
+//        while let range = range(of: string, options: options, range: start..<endIndex) {
+//            result.append(range.lowerBound)
+//            start = range.upperBound
+//        }
+//        return result
+        
+      
+        
+        decoLabel.attributedText = attributedText
     }
     
 
