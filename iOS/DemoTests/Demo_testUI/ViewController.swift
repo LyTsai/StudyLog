70b8cf8206bd9ff1623f58a3a8a72d192a46730d
LyTsai <lydire@163.com>
2020-03-09 22:06:08 +0800

    add

--- iOS/DemoTests/Demo_testUI/ViewController.swift
+++ iOS/DemoTests/Demo_testUI/ViewController.swift
@@ -10,29 +10,23 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    
+
     let imageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let scroll = UIScrollView(frame: CGRect(x: 0, y: 100, width: 300, height: 100))
-        scroll.contentSize = CGSize(width: 300, height: 500)
-        let sub = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 500))
-        sub.text = "a quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\na quick brown fox jumps over the lazy dog\n"
-        sub.numberOfLines = 0
-        sub.font = UIFont.systemFont(ofSize: 16 * minOnePoint, weight: .black)
-        scroll.addSubview(sub)
-        
-        imageView.contentMode = .scaleAspectFit
-        scroll.backgroundColor = UIColor.green
-        imageView.backgroundColor = UIColor.red
-        imageView.frame = CGRect(x: 320, y: 100, width: 300, height: 300)
-        view.addSubview(imageView)
-        view.addSubview(scroll)
-        
-        imageView.image = UIImage.imageFromView(scroll)
+        let table1 = TestTableView(frame: CGRect(x: 0, y: 120, width: 200, height: 55))
+        table1.add()
+        view.addSubview(table1)
+
+        table1.frame = CGRect(x: 0, y: 120, width: 350, height: 55)
+
+        let table3 = TestTableView(frame: CGRect(x: 0, y: 200, width: 200, height: 55))
+        table1.add()
+        view.addSubview(table3)
     }
-  
+
+
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
