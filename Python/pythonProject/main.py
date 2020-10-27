e9ee4bd7636267f73d293f1388e380f59e0a2466
LyTsai <lydire@163.com>
2020-10-27 18:06:43 +0800

    Update main.py

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-password = '1234'
-for i in range(3):
-    result = input('Input the password: ')
-    if result == password:
-        print('Login Success')
-        break
-else:
-    print("Locked")
+maxTryTime = 3
+age = 24
+
+
