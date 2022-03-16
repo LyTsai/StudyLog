e8ab7a0b7a6ac12013d60ffe025c1046eae9dffa
LyTsai <lydire@163.com>
2022-03-16 17:17:12 +0800

    update

--- webProjects/test-site/scripts/main.js
+++ webProjects/test-site/scripts/main.js
@@ -1,17 +1,80 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-// let myHTML = document.querySelector('html');
-// myHTML.addEventListener('click', function() {
-//     alert('ouch! Stop poking me!');
-// });
-// document.querySelector('html').addEventListener("click", function() {
-//     alert('ouch! Stop poking me!');
-// });
+// java script
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHigh");
 
-// document.querySelector('html').addEventListener('click', ()=> {
-//     alert('ouch! Stop poking me!');
-// });
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+guessSubmit.addEventListener("click", checkGuess);
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Last guess: ";
+  }
+  guesses.textContent += userGuess + " ";
+  
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congratulations";
+    lastResult.style.backgroundColor = "green";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!!!Game Over!!!!";
+    setGameOver();
+  }else {
+    if (userGuess < randomNumber) {
+      lastResult.textContent = "Too low";
+    }else {
+      lastResult.textContent = "Too high";
+    }
+    lastResult.style.backgroundColor = 'red';
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+}
+
+
+
+function setGameOver() {
+  
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  // if (resetButton) {
+    
+  // }
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Restart";
+  document.body.appendChild(resetButton)
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+  lastResult.style.backgroundColor = 'white';
+  lastResult.textContent = "";
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
 
 
 let myImage = document.querySelector('img');
