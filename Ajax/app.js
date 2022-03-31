a65306b98ee1cb2f653c7c1f4400a975c94b4f3b
LyTsai <lydire@163.com>
2022-03-31 15:51:15 +0800

    add

---
+++ Ajax/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// 静态资源访问服务器，放在public文件夹里
+app.use(express.static(path.join(__dirname, 'public')))
+
+// 创建路由，请求方式/地址和客户端对应
+app.get('/first', (req, res) => {
+    res.send('hello ajax')
+})
+
+app.get('/get', (req, res) => {
+    res.send(req.query)
+})
+
+app.post('/post', (req, res) => {
+    res.send(req.body)
+})
+const port = 3000
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
