0d70646df251df2803ca8b371c1455c91bc48065
LyTsai <lydire@163.com>
2022-04-12 17:20:04 +0800

    update

--- share_map_manage/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const users = require("./models/users.js")
-
-router.get('/user', users.getAllUsers)
-
-
-module.exports = router
---
+++ sharemap_backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+// models
+const User = require('./models/users')
+const queryString = require('querystringify')
+
+
+app.listen(5001, () =>
+    console.log(`Express server listening at http://localhost:5001`)
+);
