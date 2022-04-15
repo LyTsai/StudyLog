e044467b4a6840566f0613499337b62161fefd09
LyTsai <lydire@163.com>
2022-04-15 17:43:31 +0800

    update

--- sharemap_backend/models/mongoConnect.js
+++ sharemap_backend/models/mongoConnect.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 // connect
-const dbName = 'Share_map'
-const mongoUrl = "mongodb://localhost:27017/" + dbName
+const dbName = 'share_map'
+const mongoUrl = "sharemap:LyPXwvDthnPtUi6HoFzCjIfMcb5OyD4ePaVFDB5oJ4sSVQOPBQeRCMfmqwnogqPKMhWlCdI1Ed4rdnTg8SIR4A==@sharemap.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@sharemap@" + dbName
 mongoose.connect(mongoUrl, { useNewUrlParser: true})
 .then(() => {
     console.log(dbName + ' is connected')
