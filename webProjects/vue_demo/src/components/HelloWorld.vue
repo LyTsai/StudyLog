8c56f2263d796d0ce8f9ec82d40b1447630ed621
LyTsai <lydire@163.com>
2022-04-19 17:46:21 +0800

    add

---
+++ webProjects/vue_demo/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="counter++">count is: {{ counter }}, double is {{ doubleCounter}}</button>
+  <p ref="desc">dtest</p>
+</template>
+
+<script>
+import { reactive, computed, onMounted, onUnmounted, toRefs, ref, watch } from "vue"
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    const { counter, doubleCounter } = useCounter()
+    const desc = ref(null)
+    watch(() => counter, (val, oldVal) => {
+      console.log(val)
+      const p = desc.value
+      p.textContent = `counter change from ${oldVal} to ${val}`
+    })
+    return { counter, doubleCounter, desc }
+  }
+}
+
+function useCounter() {
+  https://www.bilibili.com/video/BV1WP4y1T7jS?from=search&seid=4304144511116026744&spm_id_from=333.337.0.0
+  const data = reactive({
+    counter: 1,
+    doubleCounter: computed(() => data.counter * 2)
+  })
+
+  let timer
+  onMounted(() => {
+    timer = setInterval(() => {
+      data.counter++
+    }, 1000)
+  })
+
+  onUnmounted(() => {
+    clearInterval(timer)
+  })
+  return toRefs(data)
+}
+</script>
