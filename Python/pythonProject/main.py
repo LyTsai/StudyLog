fc98dfc2daeb731af30e7f7a95e4621b9487b342
LyTsai <lydire@163.com>
2022-08-24 17:42:46 +0800

    add

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+print("Hello \\ world")
 
+print(r"Hello \\ world")
+'''
 tupleA = (2, 4, 5)
 print(tupleA[2:4])  # (5,)
 print(tupleA[::-1])  # (5, 4, 2)
 print(tupleA[::-2])  # (5, 2)
 print(tupleA[-2:-1:1])  # (4,)
 print(tupleA[-2:-1:])
-# coding=utf-8
+'''
+
+
