5a53002f94876d83b21b73f338ce8c5502ff76f6
LyTsai <lydire@163.com>
2022-09-06 17:43:57 +0800

    Update main.py

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -2,12 +2,12 @@ import requests
 import pprint
 import os
 
-url = 'https://m10.music.126.net/20220905105041/edb42d1e800e683c2bbfcd0bfe939926/ymusic/3e0e/67fe/1f4e/5132aaa36724b7bdffa99d6cb16e6e2b.mp3'   #https://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&gameId=2168&tagAll=0&page=2'
+url = 'https://img-qn.51miz.com/preview/sound/00/25/75/51miz-S257551-BE28A5AD-thumb.mp3'   #https://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&gameId=2168&tagAll=0&page=2'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36'
 }
 response = requests.get(url=url, headers=headers).content
-with open('song.mp3', mode='wb') as file:
+with open('rain.mp3', mode='wb') as file:
     file.write(response)
 # data_list = response['data']['datas']
 # for item in data_list:
