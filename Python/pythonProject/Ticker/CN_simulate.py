d1681e86cf0ca5572e9865693d34ec6181e88105
LyTsai <lydire@163.com>
2022-09-09 17:34:01 +0800

    add

--- Python/pythonProject/Ticker/CN_simulate.py
+++ Python/pythonProject/Ticker/CN_simulate.py
@@ -1,5 +1,10 @@
-numpy、pandas、scipy
+import random
 
-backtrader
+import numpy as np
 
-jupter notebook
+a = [random.uniform(100.0, 200) for i in range(50)]
+a = np.array(a)
+x = 6
+a = a * 6.5
+
+print(a.dtype)
