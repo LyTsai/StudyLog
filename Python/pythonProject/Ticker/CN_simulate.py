1d0a5b7a3bc6c3e0b0591309c479f84118a2ef43
LyTsai <lydire@163.com>
2022-09-13 00:08:24 +0800

    update

--- Python/pythonProject/Ticker/CN_simulate.py
+++ Python/pythonProject/Ticker/CN_simulate.py
@@ -2,9 +2,7 @@ import random
 
 import numpy as np
 
-a = [random.uniform(100.0, 200) for i in range(50)]
-a = np.array(a)
-x = 6
-a = a * 6.5
-
-print(a.dtype)
+# a = [random.uniform(100.0, 200) for i in range(50)]
+a = np.arange(0, 16).reshape((4, 4))
+print(a)
+print(a[1, a[1] % 2 == 0])
