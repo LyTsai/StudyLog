3f424f086e005d601a6cf30cce63c936df5f7183
LyTsai <lydire@163.com>
2022-09-14 17:48:44 +0800

    add

--- Python/pythonProject/Ticker/CN_simulate.py
+++ Python/pythonProject/Ticker/CN_simulate.py
@@ -4,5 +4,8 @@ import pandas as pd
 # a = [random.uniform(100.0, 200) for i in range(50)]
 a = np.arange(-8, 8).reshape((4, 4))
 print(a)
-a = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
-print(a[0])
+sr = pd.Series([1, 2, 3], index=['a', 'd', 'c'])
+sr1 = pd.Series([1, 2, 3], index=['a', '1', 'c'])
+df = pd.DataFrame({'one': sr, 'two': sr1})
+df.sort_index
+print(df)
