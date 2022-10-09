a72a54a0253dfd95c26df938f8303d65afdf33b9
LyTsai <lydire@163.com>
2022-10-09 17:37:28 +0800

    add

--- Python/pythonProject/Ticker/CN_simulate.py
+++ Python/pythonProject/Ticker/CN_simulate.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
+import dateutil
+
 
 # a = [random.uniform(100.0, 200) for i in range(50)]
-a = np.arange(-8, 8).reshape((4, 4))
-print(a)
-sr = pd.Series([1, 2, 3], index=['a', 'd', 'c'])
-sr1 = pd.Series([1, 2, 3], index=['a', '1', 'c'])
-df = pd.DataFrame({'one': sr, 'two': sr1})
-df.sort_index
-print(df)
+
+date = dateutil.parser.parser('2010-01-01')  # string parser time
+
+print(pd.to_datetime(['2010-01-01', '2010/Feb/02']))
