91977236f9ab062fe73dd07a237a2a6c4de90f9d
LyTsai <lydire@163.com>
2023-03-24 17:55:02 +0800

    yield

--- Steam/generator.py
+++ Steam/generator.py
@@ -18,8 +18,19 @@ def randgen(total):
     for _ in range(0, total):
         yield random.randint(1, 100)
 
-for i in Randable(20):
-  print(i)
-print('-------------------------')
-for i in randgen(20):
-  print(i)
+# for i in Randable(20):
+#   print(i)
+# print('-------------------------')
+# for i in randgen(20):
+#   print(i)
+
+
+def filter_odd(numbers):
+    for i in range(numbers):
+        if i % 2 != 0:
+            yield(i)
+
+odd_numbers = filter_odd(20)
+print(list(odd_numbers))
+for i in list(odd_numbers):
+    print(i)
