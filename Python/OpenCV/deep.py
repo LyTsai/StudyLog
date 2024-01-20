736b6c72599d9aedd92c94364e08356f8ec6c45b
LyTsai <lydire@163.com>
2024-01-20 14:28:45 +0800

    update1

--- Python/OpenCV/deep.py
+++ Python/OpenCV/deep.py
@@ -3,6 +3,8 @@ import caer
 import canaro
 import cv2 as cv
 import gc
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.utils import to_categorical
 
 IMG_SIZE = [80, 80]
 channels = 1
@@ -25,4 +27,28 @@ for i in char_dict:
 
 # train, get index - name
 train = caer.preprocess_from_dir(char_path, characters, channels=channels, IMG_SIZE=IMG_SIZE, isShuffle=True)
+len(train)
+# opencv不会在jupyter里display
+plt.figure(figsize=(80, 80))
+plt.imshow(train[0][0], cmap='gray')
+plt.show()
 
+featureSet, labels = caer.sep_train(train, IMG_SIZE=IMG_SIZE)
+# 0-1, faster
+featureSet = caer.normalize(featureSet)
+labels = to_categorical(labels, len(characters))
+
+x_train, x_val, y_train, y_val = caer.train_val_split(featureSet, labels, val_ratio=.2)
+
+del train
+del featureSet
+del labels
+gc.collect()
+
+BATCH_SIZE = 32
+EPOCHS = 10
+
+# image data generator
+datagen = canaro.generators.imageDataGenerator()
+train_gen = datagen
+# flow(x_train, y_train, batch_size)
